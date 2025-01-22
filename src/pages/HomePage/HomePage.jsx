c034ed4add7f5cd71c392f29dc911006a08953e7
tusherbhakta <tusheranta@gmail.com>
2025-01-22 23:17:47 +0530

    homepage and env update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
       </Helmet>
       {/* <Banner /> */}
       <CarouselComp />
-      <RunningMarathons />
+      {/* <RunningMarathons /> */}
       <UpcomingMarathons />
       <HowItWorks />
       <OurMission />
