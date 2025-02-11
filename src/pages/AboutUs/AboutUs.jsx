b48a63bbb7e7284b983334b91c1ea9959c83b9b0
tusherbhakta <tusheranta@gmail.com>
2025-02-11 23:04:41 +0530

    Add About Us page component

---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our company. We are dedicated to providing the best service possible.</p>
+        </div>
+    );
+};
+
+export default AboutUs;
